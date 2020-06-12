f96b321c1582cad368991e370b1502a3611b988e
FLH0202 <FLH0202@163.com>
2020-06-12 14:59:47 +0800

    11

---
+++ 6.12exercise/parameters.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+
+//选择需要演示的demo
+#define DEMO_METHOD		1			//0:yolov3 demo	1:openpose demo	
+
+//参数设置
+#define YOLOV3_VIDEO		"E:\\PIC\\testt.mp4"		
+#define OPENPOSE_VIDEO		"E:\\PIC\\testt.mp4"	
